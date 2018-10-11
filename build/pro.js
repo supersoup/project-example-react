498c72676f9c1648125f79b36434ccacce2f7260
supersoup <819127251@qq.com>
2018-10-11 17:24:34 +0800

    babel7 不能用 stage

--- build/pro.js
+++ build/pro.js
@@ -6,6 +6,8 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
     mode: 'none',
@@ -23,15 +25,58 @@ module.exports = {
             use: ExtractTextPlugin.extract({
                 fallback: "style-loader",
                 use: "css-loader"
-            }, {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
             })
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name]_[hash].[ext]',
+                    outputPath: 'image'
+                }
+            }]
+        }, {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/preset-env',
+                        '@babel/preset-react',
+                    ],
+                    "plugins": [
+                        // Stage 0
+                        "@babel/plugin-proposal-function-bind",
+        
+                        // Stage 1
+                        "@babel/plugin-proposal-export-default-from",
+                        "@babel/plugin-proposal-logical-assignment-operators",
+                        ["@babel/plugin-proposal-optional-chaining", { "loose": false }],
+                        ["@babel/plugin-proposal-pipeline-operator", { "proposal": "minimal" }],
+                        ["@babel/plugin-proposal-nullish-coalescing-operator", { "loose": false }],
+                        "@babel/plugin-proposal-do-expressions",
+        
+                        // Stage 2
+                        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+                        "@babel/plugin-proposal-function-sent",
+                        "@babel/plugin-proposal-export-namespace-from",
+                        "@babel/plugin-proposal-numeric-separator",
+                        "@babel/plugin-proposal-throw-expressions",
+        
+                        // Stage 3
+                        "@babel/plugin-syntax-dynamic-import",
+                        "@babel/plugin-syntax-import-meta",
+                        ["@babel/plugin-proposal-class-properties", { "loose": false }],
+                        "@babel/plugin-proposal-json-strings"
+                    ]
+                }
+            }]
         }]
     },
     plugins: [
+        new ManifestPlugin(),
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             template: './src/index.ejs',
             filename: 'index.html',
@@ -51,10 +96,6 @@ module.exports = {
     ],
     optimization: {
         splitChunks: {
-            // test (chunks) {
-            //     console.log(chunks);
-            //     return true;
-            // },
             cacheGroups: {
                 commons: {
                     test: /[\\/]node_modules[\\/]/,
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import fna from './a/a';
-import fnd from './d/d';
 import utilFn from './util/util1'
 import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 fna('123');
-fnd();
+
+class HelloMessage extends React.Component {
+	render() {
+		return (
+			<div>
+				Hello {this.props.name}
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<HelloMessage name="Taylor" />,
+	document.getElementById('root')
+);
